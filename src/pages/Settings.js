44eaf20b9411bf8ca27da9a9cb588913a17ad58c
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-16 15:30:03 +0000

    create Settings page

---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { Blank } from "../components";
+import { Layout } from "../components/home";
+
+export default function Settings() {
+	return (
+		<Layout>
+			<Blank>Settings</Blank>
+		</Layout>
+	);
+}
+
+const SettingsContainer = styled.div``;
