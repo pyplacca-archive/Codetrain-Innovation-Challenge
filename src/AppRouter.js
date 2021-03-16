cfdba4dba98705b9e7afe00ed7dab64f2d192b2b
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-16 18:42:10 +0000

    add authentication screens to app router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,14 @@
 import { Router } from "@reach/router";
 import { PageNotFound, Home, Settings } from "./pages";
+import { Login, Signup } from './pages/auth';
 import FormGroup from "./components/form/FormGroup";
 
 export default function AppRouter() {
 	return (
 		<Router>
 			<PageNotFound default />
+			<Signup path="/signup"/>
+			<Login path="/login"/>
 			<Home path="/" />
 			<Settings path="/settings" />
 		</Router>
