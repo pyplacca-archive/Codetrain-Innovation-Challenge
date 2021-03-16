6c74678e780b890fbe456dda1cb830ccb6dd5b56
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-16 18:41:51 +0000

    create skeleton for authentication (signup & login) screens

---
+++ src/pages/auth/Login.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+// import { AuthScreenLayout } from "../components/auth";
+
+export default function Login () {
+	return (
+		null
+	)
+}
---
+++ src/pages/auth/Signup.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+// import { AuthScreenLayout } from "../components/auth";
+
+export default function Signup () {
+	return (
+		null
+	)
+}
