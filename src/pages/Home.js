b9376f69d1f77858454ec117672a419a0406544f
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-16 15:36:19 +0000

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import Layout from "../components/home/Layout";
+import { navigate } from "@reach/router";
+
+export default function Home() {
+	return <Layout></Layout>;
+}
