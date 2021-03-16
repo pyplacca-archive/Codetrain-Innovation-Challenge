482be960ee3f1f66dcb45f04e61ff5f8c3acbe95
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-16 15:33:16 +0000

    create Navigation component for main app routes

---
+++ src/components/home/Navigation.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Search from "./Search";
+import { Bell, Cart } from "../Icons";
+import { NotificationsMenu } from "../navigation";
+
+export default function Navigation() {
+	const [showNotifications, setShowNotifications] = useState(false);
+	return (
+		<NavigationContainer>
+			<h3 className="nav-logo">Artis</h3>
+			<NavigationRight>
+				<Search />
+				<NavItem>
+					<Cart />
+				</NavItem>
+				<NavItem
+					onClick={() => setShowNotifications(!showNotifications)}
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Bell />
+					{showNotifications ? (
+						<NotificationsMenu
+							style={{
+								position: "absolute",
+								top: "2.5rem",
+								right: 0,
+							}}
+						/>
+					) : null}
+				</NavItem>
+			</NavigationRight>
+		</NavigationContainer>
+	);
+}
+
+function NavItem({ icon, children, ...props }) {
+	return <NavItemContainer {...props}>{children}</NavItemContainer>;
+}
+
+const NavigationContainer = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: var(--pad-xs) var(--pad-m);
+	padding-left: calc(var(--pad-m) * 2 + var(--icon-size));
+`;
+
+const NavigationRight = styled.ul`
+	display: flex;
+	align-items: center;
+`;
+
+const NavItemContainer = styled.li`
+	position: relative;
+	padding: var(--pad-xs) var(--pad-s);
+`;
