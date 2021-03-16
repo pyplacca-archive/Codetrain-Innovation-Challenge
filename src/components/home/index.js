4c43e5a129f4fb8e50bad28a8f179bd02c9462f8
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-16 15:30:51 +0000

    transform components/home directory into a module

---
+++ src/components/home/index.js
@@ -0,0 +1,6 @@
+import Layout from "./Layout";
+import Navigation from "./Navigation";
+import Search from "./Search";
+import Sidebar from "./Sidebar";
+
+export { Layout, Navigation, Search, Sidebar };
