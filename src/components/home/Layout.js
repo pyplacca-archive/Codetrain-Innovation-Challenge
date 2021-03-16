909c14917b95ea2bc7f55e5000fba8a58e3936ff
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-16 15:30:21 +0000

    build app layout

---
+++ src/components/home/Layout.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import Sidebar from "./Sidebar";
+import Navigation from "./Navigation";
+
+export default function Layout({ children }) {
+	return (
+		<LayoutContainer>
+			<Navigation />
+			<LayoutBottom>
+				<Sidebar />
+				<LayoutBottomRight>{children}</LayoutBottomRight>
+			</LayoutBottom>
+		</LayoutContainer>
+	);
+}
+
+const LayoutContainer = styled.main`
+	display: grid;
+	grid-template-rows: auto 1fr;
+	height: 100vh;
+`;
+
+const LayoutBottom = styled.div`
+	display: grid;
+	grid-template-columns: auto 1fr;
+	overflow-y: auto;
+`;
+
+const LayoutBottomRight = styled.div`
+	padding: var(--pad-s);
+	background-color: #ebebeb;
+	// border-radius: 20px;
+`;
