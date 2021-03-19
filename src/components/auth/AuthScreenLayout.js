59dc656a50c1fffb626258be51ba121ddc3ff73c
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-19 22:07:52 +0000

    Update AuthScreenLayout.js
    add artwork

--- src/components/auth/AuthScreenLayout.js
+++ src/components/auth/AuthScreenLayout.js
@@ -11,19 +11,25 @@ import { Footer } from "../";
 export default function AuthScreenLayout ({
 	image,
 	placeImageOnRight=false,
+	imagePosition,
+	artworkContentText,
 	showFooter=true,
+	footerColor,
 	children,
 	...props
 }) {
 	return (
-		<ScreenLayout {...{placeImageOnRight}}>
+		<ScreenLayout {...{placeImageOnRight, image, imagePos: imagePosition}}>
 			<div className="artwork">
-				{ image }
+				<img src={ image } alt=""/>
 				{
 					showFooter
-					? <Footer alignment={placeImageOnRight ? "left" : "right"} width="100%"/>
+					? <Footer alignment={placeImageOnRight ? "left" : "right"} width="100%" color={footerColor}/>
 					: null
 				}
+				{/*<div className="artwork-text">
+					{artworkContentText}
+				</div>*/}
 			</div>
 			<div className="form-section">
 				{ children }
@@ -41,10 +47,25 @@ const ScreenLayout = styled.main`
 		flex-grow: 1;
 		position: relative;
 		background-color: #f0f0f0;
+
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+			object-position: ${props => props.imagePos || "center 20%"};
+		}
+
+		.artwork-text {
+			position: absolute;
+			// bottom:
+		}
 	}
 
 	.form-section {
 		flex-basis: 35%;
+		height: 100%;
 		min-width: 500px;
+		display: flex;
+		flex-direction: column;
 	}
 `;
