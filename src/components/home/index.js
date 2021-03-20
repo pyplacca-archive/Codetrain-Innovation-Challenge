94c6a6b32e00b40fed97a064e417fe7dbe8d77fc
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-20 12:41:38 +0000

    add ProfileAvatar component to "components/home" module

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,5 +2,12 @@ import Layout from "./Layout";
 import Navigation from "./Navigation";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
+import ProfileAvatar from "./Avatar";
 
-export { Layout, Navigation, Search, Sidebar };
+export {
+	Layout,
+	Navigation,
+	Search,
+	Sidebar,
+	ProfileAvatar
+};
