36d3f3626e8b278d94238a703a6da51f7adaef51
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-21 19:20:31 +0000

    add flesh to settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,22 +1,88 @@
+import { useContext } from "react";
 import styled from "styled-components";
 import { Blank } from "../components";
 import { Layout, ProfileAvatar } from "../components/home";
+import { Switch, ProfileInfo } from "../components/settings";
+import { AppContext } from "../context";
 
 export default function Settings() {
+	const { state: {profile, preferences: {notification}}, dispatch } = useContext(AppContext);
+
+	const updateProfile = (key) => {
+		return (value) => {
+			const actionType = `update_profile_${key}`
+			dispatch({
+				type: "update_profile",
+				payload: {
+					[key]: value
+				}
+			})
+		}
+	}
+
+	const toggleNotificationPreference = (type) => {
+		return () => {
+			dispatch({
+				type: "update_notification_preference",
+				payload: {
+					[type]: !notification[type]
+				}
+			})
+		}
+	}
+
 	return (
 		<Layout>
 			<SettingsContainer>
 				<h1>Settings</h1>
 				<div>
-					<section className="profile-settings">
+					<section id="profile-settings">
 						<h3>Profile</h3>
 						<ProfileAvatar editable/>
+						<ProfileInfo
+							title="username"
+							value={profile.username}
+							onSave={updateProfile("username")}
+						/>
+						<ProfileInfo
+							title="Name"
+							value={profile.fullname}
+							onSave={updateProfile("fullname")}
+						/>
+						<ProfileInfo
+							title="email"
+							value={profile.email}
+							onSave={updateProfile("email")}
+						/>
+						<ProfileInfo
+							title="Mobile number"
+							value={profile.mobile}
+							onSave={updateProfile("mobile")}
+						/>
 					</section>
-					<div>
-						<section className="notification-settings">
+					<div className="settings-right">
+						<section id="notification-settings">
 							<h3>Notification Preferences</h3>
+							{
+								[
+									['playSound', "Play a sound when I get a notification"],
+									["desktop", "Desktop notification"],
+									["tagsIDontFollow", "Notify me on tags I don't follow"]
+								].map(pref => {
+									const [key, description] = pref;
+									return (
+										<div className="preference">
+											<Switch
+												onClick={toggleNotificationPreference(key)}
+												isOn={!!notification[key]}
+											/>
+											<span>{description}</span>
+										</div>
+									)
+								})
+							}
 						</section>
-						<section className="payment-settings">
+						<section id="payment-settings">
 							<h3>Payment Method</h3>
 						</section>
 					</div>
@@ -45,20 +111,41 @@ const SettingsContainer = styled.div`
 			padding: var(--inner-pad) 0;
 			flex-grow: 1;
 
-			> * {
-				&:nth-child(1) {
-					padding-right: var(--inner-pad);
-					border-right: 1px solid var(--artis-light-grey);
+			#profile-settings {
+				padding-right: var(--pad-l);
+				border-right: 1px solid var(--artis-light-grey);
+
+				.profile-info {
+					margin-top: var(--inner-pad);
+					margin-bottom: var(--pad-s);
+				}
+			}
+
+			.settings-right {
+				padding-left: var(--pad-l);
+
+				section:not(:last-child) {
+					margin-bottom: calc(var(--inner-pad) * 2);
 				}
 
-				&:nth-child(2) {
-					padding-left: var(--inner-pad);
+				#notification-settings {
+					.preference {
+						display: flex;
+						align-items: center;
+						margin: var(--pad-m) 0;
 
-					> * {
-						&:not(:last-child) {
-							margin-bottom: calc(var(--inner-pad) * 2);
+						> *:nth-child(1) {
+							margin-right: var(--pad-s);
+						}
+
+						span {
+							font-size: 1rem;
 						}
 					}
+				}
+
+				#payment-settings {
+
 				}
 			}
 
@@ -70,5 +157,4 @@ const SettingsContainer = styled.div`
 			}
 		}
 	}
-
 `;
