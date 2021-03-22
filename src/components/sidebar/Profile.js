31d867d5f4d0a709a337edf8d4e62cdec94fff7c
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-22 11:27:40 +0000

    build ProfileMenu component

---
+++ src/components/sidebar/Profile.js
@@ -0,0 +1,79 @@
+import { useContext } from "react";
+import styled, { keyframes } from "styled-components";
+import navigate from "@reach/router";
+import { ProfileAvatar } from "../home";
+import { AppContext } from "../../context";
+
+export default function ProfileMenu(props) {
+	const { state: { profile } } = useContext(AppContext);
+
+	return (
+		<ProfileMenuContainer {...props}>
+			<ProfileAvatar size="150px"/>
+			<div className="details">
+				<p>{profile.fullname}</p>
+				<p>{profile.email}</p>
+				<p>{profile.mobile}</p>
+			</div>
+			<button className="sign-out">Sign out</button>
+		</ProfileMenuContainer>
+	)
+}
+
+const slideRightFadeIn = keyframes`
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const ProfileMenuContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	overflow: hidden;
+	border-radius: 1rem;
+	width: 235px;
+	box-shadow: 0 1px 9px var(--shadow-color);
+	cursor: default;
+	opacity: 0;
+	transform: translateX(-1.5rem);
+	animation: ${slideRightFadeIn} 0.25s ease-out 0s forwards;
+
+	.avatar {
+		margin: var(--pad-m);
+	}
+
+	.details {
+		margin: var(--pad-m);
+		margin-top: 0;
+
+		p {
+			max-width: 24ch;
+			color: var(--artis-grey);
+			margin: var(--pad-s) 0;
+			text-align: center;
+			font-size: 1rem;
+
+			&:nth-child(1) {
+				font-size: 1.2rem;
+				font-weight: 500;
+				color: #000
+			}
+		}
+	}
+
+	.sign-out {
+		padding: var(--pad-m);
+		background-color: red /*var(--artis-grey)*/;
+		opacity: .8;
+		color: #fff;
+		width: 100%;
+		cursor: pointer;
+		transition: opacity .15s ease-in-out;
+
+		&:hover {
+			opacity: 1;
+		}
+	}
+`;
