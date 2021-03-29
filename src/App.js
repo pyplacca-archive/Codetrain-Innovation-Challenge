96ba62b2f6846dfce5ac968a02bc8cafd0f7a64a
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-03-29 22:50:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AppRouter from "./AppRouter";
 import { AppProvider } from "./context";
+import ScreensNavigator from './ScreensNavigator';
 
 function App() {
 	return (
 		<AppProvider>
 			<AppRouter />
+			<ScreensNavigator/>
 		</AppProvider>
 	);
 }
