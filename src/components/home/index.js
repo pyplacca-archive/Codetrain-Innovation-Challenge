4ea7bc66c94ed49bae3a11ea8c45927be5b90087
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-03-29 22:50:45 +0000

    update home components module

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,11 +3,15 @@ import Navigation from "./Navigation";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
 import ProfileAvatar from "./Avatar";
+import Card from "./ProductCard";
+import Preview from "./ProductPreview";
 
 export {
 	Layout,
 	Navigation,
 	Search,
 	Sidebar,
-	ProfileAvatar
+	ProfileAvatar,
+	Card,
+	Preview,
 };
