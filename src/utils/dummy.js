ba9083f5f97adc79b65eee97d743e635ee4139c0
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-03-29 22:47:15 +0000

    create dummy data

---
+++ src/utils/dummy.js
@@ -0,0 +1,18 @@
+export const data = [
+	{
+		name: 'Cardboard',
+		price: 10.00,
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, doloribus, corrupti! Quis, facilis. Quibusdam in quaerat libero, odio itaque quia expedita dignissimos excepturi! Odit, nihil.null',
+		seller: {
+			name: 'Steven Cole',
+			location: "Osu Accra",
+			image: null,
+		},
+		tags: [
+			'Drawing board',
+			'Pre-Technical Skills',
+			'Middle school',
+		],
+		id: '1q2w3e4r5t6y7u8i9o0p',
+	}
+]
