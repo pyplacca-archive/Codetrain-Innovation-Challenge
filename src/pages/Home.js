6e52f5f2b3535a1864fea446c10df98a49a75409
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-03-29 22:51:22 +0000

    update home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,46 @@
-import styled from "styled-components";
-import Layout from "../components/home/Layout";
-import { navigate } from "@reach/router";
+// third-party
+import { useState, useContext } from 'react';
+import styled from 'styled-components';
+import { navigate } from '@reach/router';
+// components
+import Layout from '../components/home/Layout';
+import { Card } from '../components/home';
+// others
+import { AppContext } from '../context';
+import { data as dummyData } from '../utils/dummy';
 
 export default function Home() {
-	return <Layout></Layout>;
+	const { dispatch } = useContext(AppContext);
+	const [selectedCard, setSelectedCard] = useState(null);
+
+	return (
+		<Layout>
+			<ProductsGrid>
+				{dummyData.map(info => (
+					<Card
+						{...info}
+						selected={selectedCard === info.id}
+						onClick={() => setSelectedCard(prev => {
+							const selection = prev !== info.id ? info : null;
+							dispatch({
+								type: 'preview_product',
+								payload: selection
+							})
+							return selection?.id;
+						})}
+						key={info.id}
+					/>
+				))}
+			</ProductsGrid>
+		</Layout>
+	);
 }
+
+const ProductsGrid = styled.div`
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
+	gap: var(--pad-m);
+	// display: grid;
+	// grid-template-columns: repeat(auto-fit, minmmax());
+`;
