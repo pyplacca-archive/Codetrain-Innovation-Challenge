77c50df0150d935e4a32378f9811afc4bb71b394
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-04-12 13:35:20 +0000

    build skeleton of notifications screen

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,6 +1,31 @@
 import { useContext } from "react";
+import styled from "styled-components";
+import Layout from '../components/home/Layout';
+import { Blank } from "../components";
 import { AppContext } from "../context";
 
 export default function NotificationsScreen() {
-	return null
+	const { state: {notifications}} = useContext(AppContext);
+
+	return (
+		<Layout>
+			<NotificationsContainer>
+				<h2>Notifications</h2>
+				{
+					!notifications.length ? (
+						<Blank>You have no notifications</Blank>
+					) : (
+						// display notifications here
+						null
+					)
+				}
+			</NotificationsContainer>
+		</Layout>
+	)
 }
+
+const NotificationsContainer = styled.div`
+	h2 {
+		margin-bottom: 1.5rem;
+	}
+`;
