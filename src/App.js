55e46c1d8965ac8a337b6f74eb4e3ab3da1e3daa
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-04-14 08:55:39 +0000

    add checkout modal to root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AppRouter from "./AppRouter";
 import { AppProvider } from "./context";
-import ScreensNavigator from './ScreensNavigator';
+import ScreensNavigator from "./ScreensNavigator";
+import { CheckoutModal } from "./components";
 
 function App() {
 	return (
 		<AppProvider>
 			<AppRouter />
+			<CheckoutModal/>
 			<ScreensNavigator/>
 		</AppProvider>
 	);
