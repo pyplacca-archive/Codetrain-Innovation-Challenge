3c03e7e0a8cc2b4c9a6c21d1f74260306bf573ce
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-04-17 16:30:50 +0000

    Update AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,8 +1,23 @@
+import { useEffect } from "react";
 import { Router } from "@reach/router";
 import { PageNotFound, Home, Settings, Notifications, Cart } from "./pages";
 import { Login, Signup } from './pages/auth';
+import { storage } from "./firebase";
 
 export default function AppRouter() {
+	// useEffect(() => {
+	// 	getAllProducts()
+	// }, [])
+
+	// const getAllProducts = () => {
+	// 	storage.child("products").listAll().then(res => {
+	// 		res.items.forEach(prod => {
+	// 			console.log({prod});
+	// 			prod.delete()
+	// 		})
+	// 	})
+	// }
+
 	return (
 		<Router>
 			<PageNotFound default />
