6f986657fc550df5d3802656c81b5cb96ddae7cb
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-04-17 16:22:48 +0000

    show search bar

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Search() {
+	const [focused, setFocused] = useState(false);
 	return (
-		<SearchContainer>
-			<span>icon</span>
-			<input type="search" />
+		<SearchContainer focused={focused}>
+			{/*<span>icon</span>*/}
+			<input
+				type="search"
+				placeholder="Search a product"
+				onFocus={() => setFocused(true)}
+				onBlur={() => setFocused(false)}
+			/>
 		</SearchContainer>
 	);
 }
 
 const SearchContainer = styled.div`
 	display: flex;
+	padding: .5rem 1rem;
+	border-radius: 1.5rem;
+	min-width: 300px;
+	border: 1px solid ${props => props.focused ? "var(--artis-blue)" : "#ccc"};
+
+	input {
+		flex-grow: 1;
+	}
 `;
