50d5462a3b7b9cf631c0f6a035550e8b3fd02e7c
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-05-01 05:12:13 +0000

    create "My products" screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import PageNotFound from "./404";
 import Home from "./Home";
 import Settings from "./Settings";
 import Cart from "./Cart";
+import MyProducts from "./MyProducts";
 import Notifications from "./Notifications";
 
 export {
@@ -9,5 +10,6 @@ export {
 	Home,
 	Settings,
 	Cart,
+	MyProducts,
 	Notifications,
 };
