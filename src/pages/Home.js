4fd68b6e70976e0774a5f815647354e0348c22ed
pyplacca <36242450+pyplacca@users.noreply.github.com>
2021-05-06 08:44:04 +0000

    update Home.js (see commit description)
    1. export ProductsGrid component for global use
    2. show loading text when first querying database for products
    3. fix grid layout issue where items sort of social distance themselves, or fill a whole row when alone

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import { navigate } from '@reach/router';
 // components
 import Layout from '../components/home/Layout';
 import { Card } from '../components/home';
+import { Blank } from '../components';
 // others
 import { AppContext } from '../context';
 import { db } from "../firebase";
@@ -12,9 +13,13 @@ import { db } from "../firebase";
 export default function Home() {
   const { state, dispatch } = useContext(AppContext);
   const [selectedCard, setSelectedCard] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     console.log({products: state.products});
+    if (state.products.length) {
+      setIsLoading(false);
+    }
   }, [state.products])
 
   useEffect(() => {
@@ -26,35 +31,40 @@ export default function Home() {
           payload: [...query.docs.map(doc => doc.data())]
         })
       })
+      .finally(() => setIsLoading(false))
     }
   }, [])
 
   return (
     <Layout>
-      <ProductsGrid>
-        {state.products.map(info => (
-          <Card
-            {...info}
-            selected={selectedCard === info.id}
-            onClick={() => setSelectedCard(prev => {
-              const selection = prev !== info.id ? info : null;
-              dispatch({
-                type: 'preview_product',
-                payload: selection
-              })
-              return selection?.id;
-            })}
-            key={info.id}
-          />
-        ))}
-      </ProductsGrid>
+      {!isLoading ? (
+        <ProductsGrid>
+          {state.products.map(info => (
+            <Card
+              {...info}
+              selected={selectedCard === info.id}
+              onClick={() => setSelectedCard(prev => {
+                const selection = prev !== info.id ? info : null;
+                dispatch({
+                  type: 'preview_product',
+                  payload: selection
+                })
+                return selection?.id;
+              })}
+              key={info.id}
+            />
+          ))}
+        </ProductsGrid>
+      ) : (
+        <Blank>Fetching products...</Blank>
+      )}
     </Layout>
   );
 }
 
-const ProductsGrid = styled.div`
+export const ProductsGrid = styled.div`
   grid-gap: var(--pad-l);
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(var(--card-width), 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(var(--card-width), 1fr));
   justify-items: center;
 `;
